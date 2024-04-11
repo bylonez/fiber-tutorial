a2f1708d786425e5c5c55ecaf0d22ee7e557a539
bylonez <bylonez@users.noreply.github.com>
2024-04-12 07:55:17 +0800

    middleware

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 	"fiber-tutorial/model"
 	_ "fiber-tutorial/service/servicedi"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"log"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"runtime/debug"
 )
 
 func main() {
@@ -17,19 +19,22 @@ func main() {
 	})
 
 	// middleware
+	// request id
+	app.Use(requestid.New())
+	// logger
+	app.Use(logger.New(logger.Config{
+		Format:     "${time} ${status} - ${method} ${path} ${locals:requestid} ${latency}\n",
+		TimeFormat: "2006-01-02T15:04:05Z07:00",
+		TimeZone:   "Asia/Shanghai",
+	}))
 	// recover from panic and log
 	app.Use(recover.New(recover.Config{
 		EnableStackTrace: true,
 		StackTraceHandler: func(c *fiber.Ctx, e interface{}) {
-			// todo log stack trace
+			log.Errorf("[PANIC RECOVER] %s\n%s", e, debug.Stack())
 		},
 	}))
-
-	app.Use(logger.New(logger.Config{
-		Format:     "${time} ${status} - ${method} ${path}\n",
-		TimeFormat: "2006-01-02T15:04:05Z07:00",
-		TimeZone:   "Asia/Shanghai",
-	}))
+	// todo logger format
 
 	handler.SetupHandler(app)
 	model.InitDB()
