2d8a74ffdee430e3948055f13ad60de2a8b9bc2c
bylonez <bylonez@users.noreply.github.com>
2024-04-18 20:14:12 +0800

    project structure

--- service/userservice/impl/service.go
+++ service/userservice/impl/service.go
@@ -9,28 +9,28 @@ import (
 type service struct {
 }
 
-func (s service) Hello3() string {
+func (s *service) Hello3() string {
 	return "hello3 result"
 }
 
-func (s service) Hello() string {
+func (s *service) Hello() string {
 	return testservice.TestService.Hello2()
 }
 
-func (s service) List(query *model.UserQuery) []*userservice.UserDTO {
+func (s *service) List(query *model.UserQuery) []*userservice.UserDTO {
 	return userservice.ToDtos(model.ListUser(query))
 }
 
-func (s service) Create(cmd *userservice.UserCreateCmd) *userservice.UserDTO {
+func (s *service) Create(cmd *userservice.UserCreateCmd) *userservice.UserDTO {
 	user := model.CreateUser(cmd.ToUser())
 	return userservice.ToDTO(user)
 }
 
-func (s service) Update(cmd *userservice.UserUpdateCmd) *userservice.UserDTO {
+func (s *service) Update(cmd *userservice.UserUpdateCmd) *userservice.UserDTO {
 	user := model.UpdateUser(cmd.ToUser())
 	return userservice.ToDTO(user)
 }
 
 func init() {
-	userservice.Service = service{}
+	userservice.Service = &service{}
 }
