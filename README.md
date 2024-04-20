4a5c4e5ebb7347443a31f2658690f4db88a914a2
bylonez <bylonez@users.noreply.github.com>
2024-04-20 17:51:30 +0800

    project structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # fiber-tutorial
+
+
+project layout inspired by [evrone/go-clean-template](https://github.com/evrone/go-clean-template)
---
+++ internal/app/app.go
@@ -0,0 +1,47 @@
+package app
+
+import (
+	"fiber-tutorial/internal/common"
+	"fiber-tutorial/internal/handler"
+	_ "fiber-tutorial/internal/model"
+	_ "fiber-tutorial/internal/service/init"
+	"github.com/gofiber/contrib/fiberzap/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/gofiber/fiber/v2/middleware/requestid"
+	"go.uber.org/zap"
+	"runtime/debug"
+)
+
+func Run() {
+	app := fiber.New(fiber.Config{
+		ErrorHandler: common.ErrorHandler,
+	})
+
+	// middleware
+	// request id
+	app.Use(requestid.New())
+	// logger
+	logger, _ := zap.NewDevelopment()
+	log.SetLogger(fiberzap.NewLogger(fiberzap.LoggerConfig{
+		SetLogger: logger,
+	}))
+	app.Use(fiberzap.New(fiberzap.Config{
+		Logger: logger,
+	}))
+	// recover from panic and log
+	app.Use(recover.New(recover.Config{
+		EnableStackTrace: true,
+		StackTraceHandler: func(c *fiber.Ctx, e interface{}) {
+			_, ok := e.(common.ErrorPanic)
+			if !ok {
+				log.Errorf("[PANIC RECOVER] %s\n%s", e, debug.Stack())
+			}
+		},
+	}))
+
+	handler.Route(app)
+
+	log.Fatal(app.Listen(":3000"))
+}
